021107c03b1119a33f9113e38ac6b63889333134
AayushN007 <hemanat2004@gmail.com>
2026-06-11 14:24:51 +0530

    Add files via upload

---
+++ prog02.py
@@ -0,0 +1,10 @@
+print("Enter a value 1")
+a = int(input())
+print("Enter a value 2")
+b = int(input())
+if(a>b):
+    print("a is greater than b")
+elif(a<b):
+    print("b is greater than a")
+else:    
+    print("a and b are equal")
